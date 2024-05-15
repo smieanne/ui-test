a6b086259d4fdb06f999b903cdfba16f909e74af
smieanne <mieannen.sugiyama@g-seed.co.jp>
2024-05-15 13:34:55 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import Button from "./components/Button";
+
+import AsyncComponent from "./components/AsyncComponent";
+
+// import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      {/* <Button label="ボタン" onClick={() => alert("click")} /> */}
+      {/* <Form /> */}
+      <AsyncComponent />
+    </>
+  );
+}
+
+export default App;
